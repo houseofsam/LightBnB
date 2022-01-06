a74e25cbcde92dba6f7d4a65fb4fb901b1bec194
houseofsam <samxsaid@gmail.com>
2022-01-06 22:37:58 +0000

    install pg and getAllProperties function to connect to DB

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,5 +1,13 @@
+const { Pool } = require('pg');
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
 
 /// Users
 
@@ -67,11 +75,13 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  return pool
+    .query(`
+      SELECT * FROM properties
+      LIMIT $1;
+    `, [limit])
+    .then((result) => result.rows)
+    .catch(err => err.message)
 }
 exports.getAllProperties = getAllProperties;
 
@@ -88,3 +98,6 @@ const addProperty = function(property) {
   return Promise.resolve(property);
 }
 exports.addProperty = addProperty;
+
+
+
